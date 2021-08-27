887c7d134eb93c416c5458ef8c77e291fd74ab72
yeonns <ysh099@gmail.com>
2021-08-27 21:32:31 +0900

    write ui test function

--- app/src/androidTest/java/com/example/android_practice/ChangeCounterBehaviorTest.kt
+++ app/src/androidTest/java/com/example/android_practice/ChangeCounterBehaviorTest.kt
@@ -1,12 +1,20 @@
 package com.example.android_practice
 
+import android.view.KeyEvent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ChangeCounterBehaviorTest {
 
     @get:Rule
@@ -22,6 +30,8 @@ class ChangeCounterBehaviorTest {
     @Test
     fun testInitCounter() {
         // Check countTV text is 0
+        onView(withId(R.id.countTV))
+            .check(matches(withText(initCounter.toString())))
     }
 
     /**
@@ -32,13 +42,21 @@ class ChangeCounterBehaviorTest {
     @Test
     fun testIncrement() {
         // Click Add button
+        onView(withId(R.id.add_button))
+            .perform(click())
         // Check countTV text is (initCounter+1)
+        onView(withId(R.id.countTV))
+            .check(matches(withText((initCounter+1).toString())))
     }
 
     @Test
     fun testDecrement() {
-        // Click Add button
+        // Click Subtract button
+        onView(withId(R.id.subtract_button))
+            .perform(click())
         // Check countTV text is (initCounter-1)
+        onView(withId(R.id.countTV))
+            .check(matches(withText((initCounter-1).toString())))
     }
 
     /**
@@ -51,15 +69,31 @@ class ChangeCounterBehaviorTest {
     @Test
     fun testChangeCounter() {
         // Edit countTV text to "changedCounter"
+        onView(withId(R.id.countTV))
+            .perform(pressKey(KeyEvent.KEYCODE_DEL), typeText(changedCounter.toString()), closeSoftKeyboard())
+
         // Click apply button
+        onView(withId(R.id.apply_button))
+            .perform(click())
+
         // Click add button
+        onView(withId(R.id.add_button))
+            .perform(click())
         // Check countTV value (changedCounter+1)
+        onView(withId(R.id.countTV))
+            .check(matches(withText((changedCounter+1).toString())))
     }
 
     @Test
     fun testChangeCounterWithoutClickApplyButton() {
         // Edit countTV text to "changedCounter"
+        onView(withId(R.id.countTV))
+            .perform(pressKey(KeyEvent.KEYCODE_DEL), typeText(changedCounter.toString()), closeSoftKeyboard())
         // Click add button
+        onView(withId(R.id.add_button))
+            .perform(click())
         // Check countTV value (initCounter + 1)
+        onView(withId(R.id.countTV))
+            .check(matches(withText((initCounter+1).toString())))
     }
 }
